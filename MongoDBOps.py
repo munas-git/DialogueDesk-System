4b94a35b25d0c0eae746d9c7934920485b03435d
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2025-01-23 11:57:31 +0000

    Fixed issue with agent and wrong answers dur to wrong tool call inputs

--- MongoDBOps.py
+++ MongoDBOps.py
@@ -4,7 +4,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # others
-import sys
+import re
 from config import *
 
 
@@ -30,9 +30,10 @@ def meetings_metadata_by_date(date: str) -> dict:
         {"no_of_meetings": *, "meeting_ids": [...]}
     """
     try:
-        result = DialogueDeskCollection.find({"Date" : date})
-
+        # formatting date to combat agent issue of passing date the wrong way.
+        result = DialogueDeskCollection.find({"Date" : date.strip("'").strip('"')})
         meeting_ids = [content["meeting_id"] for content in list(result)]
+
         output = {
             "no_of_meetings" : len(meeting_ids),
             "meeting_ids" : meeting_ids,
@@ -43,7 +44,7 @@ def meetings_metadata_by_date(date: str) -> dict:
         print(f"ERROR Retrieving date: {e}")
 
 
-def search_by_date_and_id(date:str, meeting_id:str) -> dict:
+def search_by_date_and_id(date_id_tup: tuple) -> dict:
     """
     Sample output...
     {
@@ -54,7 +55,15 @@ def search_by_date_and_id(date:str, meeting_id:str) -> dict:
     }
     """
     try:
-        result = DialogueDeskCollection.find_one({"Date" : date, "meeting_id" : meeting_id})   
+        # I think langchain agent is passing the input as string instead of actual tuple
+        # using regex to extract date and id from ... tried wraping with tuple but didnt work hence this hack (at least for now)
+
+        date_pattern, meeting_id_pattern = r"\d{4}-\d{2}-\d{2}", r"Meeting\s\d+"
+        ################# incase of errors thrown because of invalid meeting info, check back here #############
+        date, meeting_id = re.findall(date_pattern, date_id_tup)[0], re.findall(meeting_id_pattern, date_id_tup)[0]
+
+        result = DialogueDeskCollection.find_one({"Date" : date, "meeting_id" : meeting_id})
+
         output = {
             "transcript" : result["transcript"],
             "ai_summary" : result["ai_summary"],
@@ -72,6 +81,8 @@ def search_by_date_and_id(date:str, meeting_id:str) -> dict:
             "key_points" : [],
             "action_items" : [],
         }
-        return output
+        return 
     
-print(meetings_metadata_by_date("2025-01-22"))
+
+# search_by_date_and_id("2025-01-22, Meeting 1")
+# print(DialogueDeskCollection.find_one({"Date" : "2025-01-15", "meeting_id" : "Meeting 1"}))
